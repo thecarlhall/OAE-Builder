ac75bee4966a707afa90595938b3c95c575a4741
Carl Hall <carl@hallwaytech.com>
2011-06-01 09:54:33 -0400

    NYU specific settings.

---
+++ settings.rb
@@ -0,0 +1,8 @@
+nakamura = [{"path" => "../sparsemapcontent", "remote" => "nyuatlas@github", "repository" => "git@github.com:nyuatlas/sparsemapcontent.git"},
+  {"path" => "../nakamura-solr", "remote" => "nyuatlas@github", "repository" => "git@github.com:nyuatlas/solr.git"},
+  {"path" => "../nakamura", "remote" => "nyuatlas@github", "repository" => "git@github.com:nyuatlas/nakamura.git"}]
+ui = {"path" => "../3akai-ux", "remote" => "nyuatlas@github", "repository" => "git@github.com:nyuatlas/3akai-ux.git"}
+
+#APP_OPTS = "-f - > app.log 2>&1"
+JAVA_DEBUG = true
+MVN_OPTS = "-Dmaven.repo.local=/home/chall/projects/nyu/repository"
