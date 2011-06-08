dfb1071e32d24965a0b694c7747f776a3e78c851
Carl Hall <carl@hallwaytech.com>
2011-06-08 13:35:23 -0400

    Removed port 8181

--- settings.rb
+++ settings.rb
@@ -3,6 +3,6 @@ nakamura = [{"path" => "../sparsemapcontent", "remote" => "nyuatlas@github", "re
   {"path" => "../nakamura", "remote" => "nyuatlas@github", "repository" => "git@github.com:nyuatlas/nakamura.git"}]
 ui = {"path" => "../3akai-ux", "remote" => "nyuatlas@github", "repository" => "git@github.com:nyuatlas/3akai-ux.git"}
 
-APP_OPTS = "-p 8181" 
+#APP_OPTS = "-p 8181" 
 JAVA_DEBUG_OPTS = "-Xdebug -Xrunjdwp:transport=dt_socket,address=8600,server=y,suspend=n"
 MVN_OPTS = "-Dmaven.repo.local=/home/chall/projects/nyu/repository"
